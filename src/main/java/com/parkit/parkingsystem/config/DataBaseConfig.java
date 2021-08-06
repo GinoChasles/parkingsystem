0bb0cb1ea5e634a3a77f3c4c5597165d22846344
ginochasles <ginochasles@gmail.com>
2021-08-06 10:35:39 +0200

    modification nom log4j.properties et correctifs class App suivant checkstyle

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -13,7 +13,7 @@ public class DataBaseConfig {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod", System.getenv("MYSQL_USER"), System.getenv("MYSQL_PASSWORD"));
+                "jdbc:mysql://localhost:3306/prod?serverTimezone=UTC", System.getenv("MYSQL_USER"), System.getenv("MYSQL_PASSWORD"));
     }
 
     public void closeConnection(Connection con) {
