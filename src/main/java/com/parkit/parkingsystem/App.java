03f0b0934956edb47c65802b7d291f657a191f1d
ginochasles <ginochasles@gmail.com>
2021-08-20 10:47:49 +0200

    ajout javadoc app

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -4,8 +4,16 @@ import com.parkit.parkingsystem.service.InteractiveShell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class App {
+public final class App {
+    /**
+     * Logger de l'application.
+     */
     private static final Logger LOGGER = LogManager.getLogger("App");
+
+    /**
+     * Lancement de l'application.
+     * @param args
+     */
     public static void main(final String[] args) {
         LOGGER.info("Initializing Parking System");
         InteractiveShell.loadInterface();
