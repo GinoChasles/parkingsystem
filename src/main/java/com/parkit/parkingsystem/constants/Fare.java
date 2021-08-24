23af6be86fd15ce336a62827b5f78ed6b1495f56
ginochasles <ginochasles@gmail.com>
2021-08-24 17:37:00 +0200

    Les classes utilitaires ne doivent pas avoir de constructeur par défaut ou public: Fare.java: résolution par final class et déclaration d'un private Fare

--- src/main/java/com/parkit/parkingsystem/constants/Fare.java
+++ src/main/java/com/parkit/parkingsystem/constants/Fare.java
@@ -1,6 +1,6 @@
 package com.parkit.parkingsystem.constants;
 
-public class Fare {
+public final class Fare {
     /**
      * Prix de stationnement par heure d'une moto.
      */
@@ -10,4 +10,7 @@ public class Fare {
      * Prix de stationnement par heure d'une voiture.
      */
     public static final double CAR_RATE_PER_HOUR = 1.5;
+
+    private Fare() { }
+
 }
